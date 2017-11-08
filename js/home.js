fbb3e1afbc6d03247d46b74a6cc2eaab564f080a
Petrus Krüger <petruskruger81@gmail.com>
2017-11-08 07:59:24 +0200

    completed login page

--- js/home.js
+++ js/home.js
@@ -13,3 +13,28 @@ function hamMenu(x) {
     x.classList.toggle("change");
     document.getElementById("dropmenu").classList.toggle("show");
 };
+
+//functionality of the slider
+
+var image = ["firstimage.PNG", "secondimage.png", "thirdimage.jpg"];
+var i = 0;
+
+function sliderfunction() {
+    var whatever = setTimeout(move, 3000);
+}
+
+function move() {
+    for (i = 0; i < image.length; i++) {
+        alert("here");
+        document.getElementsByClassName("slider")[0].setAttribute("src", image[i]);
+        x = i + 1;
+        document.getElementById("title").innerHTML = "Slider " + x + "!";
+    }
+    
+    
+    //console.log(x);
+    //var y = document.getElementById("title").innerHTML = "Slide 2";
+    
+    //x.setAttribute("src", "secondimage.png");
+}
+
